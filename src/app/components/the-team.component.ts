164e040c5a2f425367ac1c330852d4966bbe2aa6
Q <tipster74743@gmail.com>
2024-08-30 20:20:14 -0500

    Update new positions and remove old events

--- src/app/components/the-team.component.ts
+++ src/app/components/the-team.component.ts
@@ -31,16 +31,16 @@ import { ProfileCardComponent } from './profile-card.component';
 export class TheTeamComponent {
   team = [
     {
-      image: '/profile-pics/jared.jpeg',
-      title: 'President',
-      name: 'Jared',
-      bio: 'Jared is the one who keeps us focused on our mission, makes sure that we act with compassion, and that we make a better NorthWest Arkansas in the process. From Mega Board Game Conventions to family gatherings, you can find Jared seeking to play games with people he’s never met before.'
-    },
-    {
       image: '/profile-pics/jesse.jpg',
-      title: 'Vice-President',
+      title: 'President',
       name: 'Jesse',
       bio: 'Jesse is the catalyst for the forming of Meeple Academy of the Ozarks, and a cheerleader of sorts! He and his wife have experience running small businesses, and with that experience he reminds us that we can make a difference. Without Jesse, we wouldn’t have the determination that we do now!'
+    },
+    {
+      image: '/profile-pics/clancy.jpg',
+      title: 'Vice-President',
+      name: 'Clancy',
+      bio: 'Clancy wears many hats as the Library Curator. Primarily, he’s in charge of putting together a robust library of board games that appeals to board gamers at all levels and from all walks of life. He’s also set up all of our tools for communication and organization. And last of all, he’s a sweetheart. You could tell him, “I’m going to destroy you [in this game],” and he’d simply reply “I like your spirit!”'
     },
     {
       image: '/profile-pics/q.png',
@@ -50,21 +50,21 @@ export class TheTeamComponent {
     },
     {
       image: '/profile-pics/liz.jpeg',
-      title: 'Marketing Director',
+      title: 'Community Outreach Director',
       name: 'Liz',
       bio: 'The writer of most of Meeple Academy’s online content and emails. Talks a lot too. She’s alright.'
-    },
-    {
-      image: '/profile-pics/clancy.jpg',
-      title: 'Development Director',
-      name: 'Clancy',
-      bio: 'Clancy wears many hats as the Library Curator. Primarily, he’s in charge of putting together a robust library of board games that appeals to board gamers at all levels and from all walks of life. He’s also set up all of our tools for communication and organization. And last of all, he’s a sweetheart. You could tell him, “I’m going to destroy you [in this game],” and he’d simply reply “I like your spirit!”'
     },
     {
       image: '/profile-pics/heather.jpeg',
       title: 'Financial Director',
       name: 'Heather',
       bio: 'Our fiscal guru who’s on top of keeping us on track to host board game events, and making sure that we conduct ourselves properly as a nonprofit in good standing! It’s not the spiciest job, but it’s one that requires a smart woman with experience and training. And Heather’s the right one for us. She runs a monthly gathering with her doting husband dubbed “Castle Adams,” and is raising two ancient dogs that wear diapers.'
+    },
+    {
+      image: '/profile-pics/meeple.png',
+      title: 'Marketing Director',
+      name: 'Jessica',
+      bio: `Jessica is our newest member and doesn't exactly have a cool blurb like the rest of us :(`
     }
   ];
 }
--- src/assets/events.ts
+++ src/assets/events.ts
@@ -15,19 +15,6 @@ export interface GameEvent {
 export const events: GameEvent[] = [
   {
     eventId: 1,
-    eventName: 'Board Game Night at the Botanical',
-    eventDate: 'Every Monday',
-    eventTime: '6:00 PM - 11:00 PM',
-    eventImage: '/events/botanical.jpeg',
-    altDesc: 'Botanical Game Nights on Mondays',
-    eventDescription: 'Come enjoy a chill night of boardgames at one of the best atmospheres in Bentonville.',
-    about: 'Join us for an evening of fun, wine, and premium draft beers at a great location in Downtown Bentonville. This weekly event is a great opportunity to connect with like-minded individuals in your community and build meaningful relationships in a relaxed and welcoming environment.',
-    venueName: 'The Botanical',
-    venueSite: 'https://www.botanicalnwa.com/',
-    venueAddress: '713 SW A St, Bentonville, AR 72712'
-  },
-  {
-    eventId: 2,
     eventName: 'Board Games at Fay Beerworks',
     eventDate: 'Every Tuesday',
     eventTime: '6:00 PM - 9:00 PM',
@@ -40,13 +27,13 @@ export const events: GameEvent[] = [
     venueAddress: '2649 E Mission Blvd #1, Fayetteville, AR 72703'
   },
   {
-    eventId: 3,
+    eventId: 2,
     eventName: 'AdventureCon',
-    eventDate: 'May 24 - May 26',
-    eventTime: '9:00 AM - 11:00 PM',
+    eventDate: 'Coming Soon!',
+    eventTime: '9:00 AM - 12:00 PM',
     eventImage: '/events/adventure-con.png',
     altDesc: 'AdventureCon by Meeple Academy',
-    eventDescription: 'Come enjoy a drink and play a game',
+    eventDescription: 'Our yearly capstone event.',
     about: 'The Best Weekend of Gaming in Northwest Arkansas',
     venueName: 'Four Points by Sheraton',
     venueSite: 'www.adventure-con.com',
